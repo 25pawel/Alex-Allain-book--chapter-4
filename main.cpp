30a64bbdf7e10ba0967ca351fd316aa95a1af2ce
Pawel <25pawel@gmail.com>
2022-06-30 07:58:29 +0200

    Alex Allain - chapter 4 - tasks

--- main.cpp
+++ main.cpp
@@ -4,10 +4,103 @@
 using namespace std;
 
 static void compareWhoIsOlder(void);
+static void verifyIntegerPassword(void);
+static void launchCalculator(void);
+static void logIntoSystem(void);
 
 int main()
 {
-    compareWhoIsOlder();
+    // compareWhoIsOlder();
+    // verifyIntegerPassword();
+    // launchCalculator();
+    logIntoSystem();
+}
+
+static void logIntoSystem(void)
+{
+    string name;
+    string password;
+
+    cout << "Enter name: ";
+    cin >> name;
+
+    cout << "Enter password: ";
+    cin >> password;
+
+    if((name == "admin1") && (password == "pass1"))
+    {
+        cout << name << " logged into system\n";
+    }
+    else if ((name == "admin2") && (password == "pass2"))
+    {
+        cout << name << " logged into system\n";
+    }
+    else if ((name == "admin3") && (password == "pass3"))
+    {
+        cout << name << " logged into system\n";
+    }
+    else
+    {
+        logIntoSystem();
+    }
+}
+
+static void launchCalculator(void)
+{
+    char operation;
+    int firstArgument;
+    int secondArgument;
+
+    cout << "Enter first argument: ";
+    cin >> firstArgument;
+
+    cout << "Enter second argument: ";
+    cin >> secondArgument;
+
+    cout << "Enter operation: ";
+    cin >> operation;
+
+    if(operation == '+')
+    {
+        cout << firstArgument << " + " << secondArgument << " = " << (firstArgument + secondArgument);
+    }
+    else if (operation == '-')
+    {
+        cout << firstArgument << " - " << secondArgument << " = " << (firstArgument - secondArgument);
+    }
+    else if (operation == '*')
+    {
+        cout << firstArgument << " * " << secondArgument << " = " << (firstArgument * secondArgument);
+    }
+    else if (operation == '/')
+    {
+        cout << firstArgument << " / " << secondArgument << " = " << (firstArgument / secondArgument);
+    }
+    else
+    {
+        cout << "Operation not recognized\n";
+    }
+}
+
+static void verifyIntegerPassword(void)
+{
+    int firstPassword;
+    int secondPassword;
+
+    cout << "Enter first password: ";
+    cin >> firstPassword;
+
+    cout << "Enter second password: ";
+    cin >> secondPassword;
+
+    if((firstPassword == 0) || secondPassword == 1234)
+    {
+        cout << "Password correct";
+    }
+    else
+    {
+        cout << "Password incorrect";
+    }
 }
 
 static void compareWhoIsOlder(void)
